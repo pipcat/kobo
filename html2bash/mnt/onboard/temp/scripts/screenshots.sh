07fa6f7d33ed05fbe0e01f64daa318f0c4feef12
pipcat <pip@pipcat.com>
2016-06-29 18:44:36 +0200

    Delete screenshots.sh

--- html2bash/mnt/onboard/temp/scripts/screenshots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Change Screenshots=true/false in ﻿[FeatureSettings] for Kobo eReader.conf
-
-KOBO_CONF="/mnt/onboard/.kobo/Kobo/Kobo eReader.conf"
-
-sed 's/^Screenshots=true$/Screenshots=false/g' <"$KOBO_CONF" >"$KOBO_CONF".tmp && mv "$KOBO_CONF".tmp "$KOBO_CONF"
